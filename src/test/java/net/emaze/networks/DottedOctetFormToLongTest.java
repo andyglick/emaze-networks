43999d1dd6986d5615edc8e3e641a96b0f5f1bc2
Simone Baruzza <simone.baruzza@emaze.net>
2013-03-06 11:59:30 +0100

    enh: DottedFormToLong now allows dec, hex, oct representation of IPs

--- src/test/java/net/emaze/networks/DottedOctetFormToLongTest.java
+++ src/test/java/net/emaze/networks/DottedOctetFormToLongTest.java
@@ -1,10 +1,11 @@
 package net.emaze.networks;
 
 import junit.framework.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class DottedOctetFormToLongTest {
-    
+
     private final static long LOCALHOST_AS_LONG = 2130706433L;
 
     @Test(expected = IllegalArgumentException.class)
@@ -25,6 +26,24 @@ public class DottedOctetFormToLongTest {
     @Test
     public void parsingAValidDottedOctetFormStringYieldsExpected() {
         final long longForm = new DottedOctetFormToLong().perform("127.0.0.1");
+        Assert.assertEquals(LOCALHOST_AS_LONG, longForm);
+    }
+
+    @Test
+    public void parsingAValidDottedOctetFormHexStringYieldsExpected() {
+        final long longForm = new DottedOctetFormToLong().perform("0x7F.0x00.0x00.0x01");
+        Assert.assertEquals(LOCALHOST_AS_LONG, longForm);
+    }
+
+    @Test
+    public void parsingAValidDottedOctetFormOctStringYieldsExpected() {
+        final long longForm = new DottedOctetFormToLong().perform("0177.0000.0000.0001");
+        Assert.assertEquals(LOCALHOST_AS_LONG, longForm);
+    }
+
+    @Test
+    public void parsingAValidDottedOctetFormMixedStringYieldsExpected() {
+        final long longForm = new DottedOctetFormToLong().perform("0177.0x00.0.0X01");
         Assert.assertEquals(LOCALHOST_AS_LONG, longForm);
     }
 }
