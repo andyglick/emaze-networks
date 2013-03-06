2101d9162f265f89e5a8c2debbea7cab481b47e5
Simone Baruzza <simone.baruzza@emaze.net>
2013-03-06 11:46:23 +0100

    ref: removing useless cast

--- src/main/java/net/emaze/networks/LongToBits.java
+++ src/main/java/net/emaze/networks/LongToBits.java
@@ -11,19 +11,17 @@ import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 import net.emaze.dysfunctional.dispatching.delegates.Identity;
 import net.emaze.dysfunctional.dispatching.logic.Predicate;
 
-
 public class LongToBits implements Delegate<Integer, Long> {
-    
+
     private final Predicate<Boolean> isTrue = Dispatching.predicate(new Identity<Boolean>());
     private final Predicate<Boolean> isFalse = Logic.not(isTrue);
-    
+
     @Override
     public Integer perform(Long longForm) {
         dbc.precondition(longForm != null, "longForm cannot be null");
-        final List<Boolean> bits =  Consumers.all(Filtering.takeLast(32, new LongBitsIterator(longForm)));
-        final int onesCount = (int)Reductions.count(Filtering.takeWhile(bits, isTrue));
+        final List<Boolean> bits = Consumers.all(Filtering.takeLast(32, new LongBitsIterator(longForm)));
+        final int onesCount = Reductions.counti(Filtering.takeWhile(bits, isTrue));
         dbc.precondition(Reductions.every(Filtering.takeLast(32 - onesCount, bits), isFalse), "Noncontinuous '1' bits present");
         return onesCount;
     }
-
 }
