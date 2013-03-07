ded03df8ecc6ac283bc89a4ea0049551b850e1b9
Simone Baruzza <simone.baruzza@emaze.net>
2013-03-07 12:35:13 +0100

    ref: removed useless methods

--- src/main/java/net/emaze/networks/Ipv4SequencingPolicy.java
+++ src/main/java/net/emaze/networks/Ipv4SequencingPolicy.java
@@ -5,15 +5,12 @@ import net.emaze.dysfunctional.order.SequencingPolicy;
 
 public class Ipv4SequencingPolicy implements SequencingPolicy<Ipv4> {
 
+    @Override
     public Maybe<Ipv4> next(Ipv4 element) {
         if (Ipv4.LAST_IP == element) {
             return Maybe.nothing();
         }
         return Maybe.just(element.next());
-    }
-
-    public Ipv4 prev(Ipv4 element) {
-        return element.previous();
     }
 
     @Override
--- src/test/java/net/emaze/networks/Ipv4SequencingPolicyTest.java
+++ src/test/java/net/emaze/networks/Ipv4SequencingPolicyTest.java
@@ -17,16 +17,5 @@ public class Ipv4SequencingPolicyTest {
         final Maybe<Ipv4> got = new Ipv4SequencingPolicy().next(Ipv4.LAST_IP);
         Assert.assertFalse(got.hasValue());
     }
-    
-    @Test
-    public void prevYieldsPreviousIp() {
-        final Ipv4 got = new Ipv4SequencingPolicy().prev(Ipv4.parse("10.0.0.1"));
-        Assert.assertEquals(Ipv4.parse("10.0.0.0"), got);
-    }
-    
-    @Test(expected = IllegalStateException.class)
-    public void prevYieldsExceptionAppliedToFirstIp() {
-        new Ipv4SequencingPolicy().prev(Ipv4.FIRST_IP);
-    }
 }
 
