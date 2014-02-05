7f6e58ccee594d67a27d5ba7c0fac4bf3352897e
Lorenzo Caenazzo <lorenzo.caenazzo@emaze.net>
2014-02-05 18:06:31 +0100

    enh: coverage

---
+++ src/test/java/net/emaze/networks/Ipv6SubtractIpFromNetworkTest.java
@@ -0,0 +1,57 @@
+package net.emaze.networks;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class Ipv6SubtractIpFromNetworkTest {
+
+    public static final Ipv6SubtractIpFromNetwork subtractor = new Ipv6SubtractIpFromNetwork();
+
+    @Test
+    public void excludingIpOutsideCidrYieldsCidr() {
+        final Ipv6Network cidr = Ipv6Network.fromCidrNotation("1234::/16");
+        final Set<Ipv6Network> got = subtractor.perform(cidr, Ipv6.parse("4321::"));
+        Assert.assertEquals(Collections.singleton(cidr), got);
+    }
+
+    @Test
+    public void excludingAllCidrContentsYieldsEmptySet() {
+        final Ipv6Network cidr = Ipv6Network.fromCidrNotation("1234::1/128");
+        final Set<Ipv6Network> got = subtractor.perform(cidr, Ipv6.parse("1234::1"));
+        Assert.assertEquals(Collections.emptySet(), got);
+    }
+
+    @Test
+    public void excludingFirstIpYieldsExpected() {
+        final Ipv6Network cidr = Ipv6Network.fromCidrNotation("1234::", 126);
+        final Set<Ipv6Network> got = subtractor.perform(cidr, Ipv6.parse("1234::"));
+        final Set<Ipv6Network> expected = new HashSet<>(Arrays.asList(
+                Ipv6Network.fromCidrNotation("1234::1", 128),
+                Ipv6Network.fromCidrNotation("1234::2", 127)));
+        org.junit.Assert.assertEquals(expected, got);
+    }
+
+    @Test
+    public void excludingMiddleIpYieldsExpected() {
+        final Ipv6Network cidr = Ipv6Network.fromCidrNotation("1234::", 126);
+        final Set<Ipv6Network> got = subtractor.perform(cidr, Ipv6.parse("1234::1"));
+        final Set<Ipv6Network> expected = new HashSet<>(Arrays.asList(
+                Ipv6Network.fromCidrNotation("1234::", 128),
+                Ipv6Network.fromCidrNotation("1234::2", 127)));
+        org.junit.Assert.assertEquals(expected, got);
+    }
+
+    @Test
+    public void excludingLastIpYieldsExpected() {
+        final Ipv6Network cidr = Ipv6Network.fromCidrNotation("1234::", 126);
+        final Set<Ipv6Network> got = subtractor.perform(cidr, Ipv6.parse("1234::3"));
+        final Set<Ipv6Network> expected = new HashSet<>(Arrays.asList(
+                Ipv6Network.fromCidrNotation("1234::2", 128),
+                Ipv6Network.fromCidrNotation("1234::", 127)));
+        org.junit.Assert.assertEquals(expected, got);
+    }
+}
---
+++ src/test/java/net/emaze/networks/Ipv6SubtractNetworksFromNetworkTest.java
@@ -0,0 +1,62 @@
+package net.emaze.networks;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class Ipv6SubtractNetworksFromNetworkTest {
+
+    @Test
+    public void canSubtractContainedFromContainer() {
+        final Ipv6Network minuend = Ipv6Network.fromCidrNotation("1234::/16");
+        final Ipv6Network subtrahend = Ipv6Network.fromCidrNotation("1234::/17");
+        final Ipv6Network expected = Ipv6Network.fromCidrNotation("1234:8000:/17");
+        Assert.assertEquals(Collections.singleton(expected), new Ipv6SubtractNetworksFromNetwork().perform(minuend, Collections.singleton(subtrahend)));
+    }
+
+    @Test
+    public void canSubtractCidrFromItself() {
+        final Ipv6Network cidr = Ipv6Network.fromCidrNotation("1234::/16");
+        Assert.assertEquals(Collections.emptySet(), new Ipv6SubtractNetworksFromNetwork().perform(cidr, Collections.singleton(cidr)));
+    }
+
+    @Test
+    public void subtractingNonOverlappingRangesYieldsMinuend() {
+        final Ipv6Network minuend = Ipv6Network.fromCidrNotation("1234::/16");
+        final Ipv6Network nonOverlappingSubtrahend = Ipv6Network.fromCidrNotation("4321::/16");
+        Assert.assertEquals(Collections.singleton(minuend), new Ipv6SubtractNetworksFromNetwork().perform(minuend, Collections.singleton(nonOverlappingSubtrahend)));
+    }
+
+    @Test
+    public void subtractingContainerFromContainedYieldsEmptySet() {
+        final Ipv6Network minuend = Ipv6Network.fromCidrNotation("1234::/17");
+        final Ipv6Network subtrahend = Ipv6Network.fromCidrNotation("1234::/16");
+        Assert.assertEquals(Collections.emptySet(), new Ipv6SubtractNetworksFromNetwork().perform(minuend, Collections.singleton(subtrahend)));
+    }
+
+    @Test
+    public void SubtractingSmallCidrFromBigCidrYieldsLotsOfCidrs() {
+        final Ipv6Network minuend = Ipv6Network.fromCidrNotation("1234::/16");
+        final Ipv6Network subtrahend = Ipv6Network.fromCidrNotation("1234::/20");
+        final Set<Ipv6Network> expected = new HashSet<>(Arrays.asList(
+                Ipv6Network.fromCidrNotation("1234:8000:/17"),
+                Ipv6Network.fromCidrNotation("1234:1000:/20"),
+                Ipv6Network.fromCidrNotation("1234:2000:/19"),
+                Ipv6Network.fromCidrNotation("1234:4000:/18")
+        ));
+        Assert.assertEquals(expected, new Ipv6SubtractNetworksFromNetwork().perform(minuend, Collections.singleton(subtrahend)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullMinuendThrows() {
+        new Ipv6SubtractNetworksFromNetwork().perform(null, Collections.<Ipv6Network>emptySet());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullSubtrahendThrows() {
+        new Ipv6SubtractNetworksFromNetwork().perform(Ipv6Network.fromCidrNotation("1234::/16"), null);
+    }
+}
