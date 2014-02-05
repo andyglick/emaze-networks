b77d7839104f42f6a65877deaa4a2e6995efa0ee
Simone Baruzza <simone.baruzza@emaze.net>
2014-02-05 16:53:26 +0100

    fix: IpRanges cannot be instantiated

--- src/main/java/net/emaze/networks/IpRanges.java
+++ src/main/java/net/emaze/networks/IpRanges.java
@@ -3,7 +3,7 @@ package net.emaze.networks;
 import net.emaze.dysfunctional.Ranges;
 import net.emaze.dysfunctional.order.ComparableComparator;
 
-public class IpRanges {
+public abstract class IpRanges {
 
     public static Ranges<Ipv4> RANGESV4 = new Ranges<>(new ComparableComparator<Ipv4>(), new Ipv4SequencingPolicy(), Ipv4.getFirstIp());
     public static Ranges<Ipv6> RANGESV6 = new Ranges<>(new ComparableComparator<Ipv6>(), new Ipv6SequencingPolicy(), Ipv6.getFirstIp());
