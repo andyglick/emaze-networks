dadf27bdc16ab6aeb29b9fa8a69714a9626703ba
Simone Baruzza <simone.baruzza@emaze.net>
2014-02-10 11:27:03 +0100

    enh: network splitter tests

--- src/main/java/net/emaze/networks/Ipv6NetworkSplitter.java
+++ src/main/java/net/emaze/networks/Ipv6NetworkSplitter.java
@@ -15,6 +15,8 @@ public class Ipv6NetworkSplitter implements BinaryDelegate<List<Ipv6Network>, Ip
         final Ipv6Mask mask = Ipv6Mask.net(newNetmaskSize);
         final FixedSizeNatural base = source.firstIp().bits();
         final int bits = newNetmaskSize - source.netmask().population();
+        dbc.precondition(bits >= 0, "Target netmask must be narrower than source netmask");
+        dbc.precondition(bits <= 32, "Cannot split with a delta bigger than 32 bits");
         final FixedSizeNatural last = FixedSizeNatural.one(128).shiftLeft(bits);
         for (FixedSizeNatural delta = FixedSizeNatural.zero(128); !delta.equals(last); delta = delta.increment()) {
             final FixedSizeNatural address = base.or(delta.shiftLeft(128 - newNetmaskSize));
